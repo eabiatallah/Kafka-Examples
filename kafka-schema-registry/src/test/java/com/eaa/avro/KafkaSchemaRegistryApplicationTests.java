4d1ef33ed29c9b07cad7a11e311ada225283acd8
eabiatallah <eabiatallah@gmail.com>
2024-12-30 22:50:13 -0500

    kafka avro schema registry

---
+++ kafka-schema-registry/src/test/java/com/eaa/avro/KafkaSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaa.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSchemaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
